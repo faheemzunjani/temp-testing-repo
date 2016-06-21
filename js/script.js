d4b49d6244b287ce9d6c039f5a44d0257790bef8
Faheem Zunjani <faheemzunjani@gmail.com>
2016-06-21 10:40:36 +0530

    https instead of http

--- js/script.js
+++ js/script.js
@@ -14,10 +14,10 @@
     var dc = {};
 
     var homeHtmlUrl = "snippets/home-snippet.html";
-    var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+    var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
-    var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
